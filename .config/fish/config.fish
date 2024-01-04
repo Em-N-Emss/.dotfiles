e01217b69130b48bdd488d9ea80e729dddfdcc6b
EmRenRen <sengchanh.dylan@gmail.com>
2024-01-04 16:51:39 +0100

    Update Path Fish and commentaries

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,15 +11,21 @@ set -g theme_hostname always
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    # Active Brew
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 
+    # Navigation de fichiers
     alias ls "ls -p -G"
     alias la "ls -A"
+
     command -qv nvim && alias vim nvim
+
+    # Alias pour .dotfiles sur git
     alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
     set -gx EDITOR nvim
 
+    # Utilisation d'eza comme exa est plus entretenu
     if type -q eza
         alias ll "eza -l -g --icons"
         alias lla "ll -a"
@@ -27,15 +33,21 @@ if status is-interactive
 end
 
 
-set -gx PATH bin $PATH
-set -gx PATH ~/bin $PATH
-set -gx PATH ~/.local/bin $PATH
+# set -gx PATH bin $PATH
+# set -gx PATH ~/bin $PATH
+# set -gx PATH ~/.local/bin $PATH
+fish_add_path bin
+fish_add_path ~/bin
+fish_add_path ~/.local/bin
 
 function ktstart
     # Check si komorebi n'est pas déjà démarré
     if not pgrep -f komorebic >/dev/null
         komorebic start -c (wslpath -w "/mnt/c/Users/imran/komorebi.json") --whkd $argv
     end
+
+    # Laisse le temps au terminal de respirer
+    sleep 1
 
     # Lance Tmux avec le nom de la session Em
     tmux new -s Em
@@ -44,6 +56,9 @@ end
 function ktend
     # Byebye Komorebi
     komorebic stop
+
+    # Laisse le temps au terminal de respirer
+    sleep 1
 
     # Ferme toutes les sessions Tmux
     tmux kill-server

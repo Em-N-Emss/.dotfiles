94635570a9bf6940d692501a70bb147624a3421e
Em-N-Ems <sengchanh.dylan@gmail.com>
2024-02-08 20:36:06 +0100

    feat: add news directory for fzf

--- .config/fish/functions/fzf_change_directory.fish
+++ .config/fish/functions/fzf_change_directory.fish
@@ -13,6 +13,10 @@ function fzf_change_directory
     begin
         echo $HOME/.config
         # find $(ghq root) -maxdepth 4 -type d -name .git | sed 's/\/\.git//'
+        # WSL
+        ls -ad /mnt/c/Users/*/.*
+        ls -ad /mnt/c/Divers/*
+        ls -ad $HOME/.config/*/*
         ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git
         ls -ad $HOME/Developments/*/* | grep -v \.git
     end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv

6aa4693827ae713e01ba3244f31d9f528376458d
Em-N-Ems <sengchanh.dylan@gmail.com>
2024-03-09 01:40:26 +0100

    feat(config.fish): add rose pine to fzf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,14 @@ set -g theme_display_user yes
 set -g theme_hide_hostname no
 set -g theme_hostname always
 
+# Rose pine pour fzf
+set -Ux FZF_DEFAULT_OPTS "
+	--color=fg:#908caa,bg:#191724,hl:#ebbcba
+	--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
+	--color=border:#403d52,header:#31748f,gutter:#191724
+	--color=spinner:#f6c177,info:#9ccfd8,separator:#403d52
+	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa"
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
     # Active Brew

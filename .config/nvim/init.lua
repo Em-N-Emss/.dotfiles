f42d1783647458ba01e91b4334c47c26fbbd3af1
Em-N-Ems <sengchanh.dylan@gmail.com>
2024-03-13 14:53:11 +0100

    feat(init.lua, tmux.conf): minimal display for tmux and VIM

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 if vim.loader then
-	vim.loader.enable()
+    vim.loader.enable()
 end
 
 _G.dd = function(...)
-	require("util.debug").dump(...)
+    require("util.debug").dump(...)
 end
 vim.print = _G.dd
 
 require("config.lazy")
+
+-- Background et Foreground pour la statusline VIM sans lualine
+vim.cmd("highlight StatusLine ctermbg=1f1d2e guibg=#1f1d2e")
+vim.cmd("highlight StatusLine ctermfg=908caa guifg=#908caa")

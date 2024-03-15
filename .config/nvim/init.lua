8d21ec34040336290df29d807dc9e82ee4a66ef5
Em-N-Ems <sengchanh.dylan@gmail.com>
2024-03-15 01:45:22 +0100

    feat(init.lua tmux.conf): changes for statusline and tmux apparence

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,6 +10,59 @@ vim.print = _G.dd
 
 require("config.lazy")
 
+-- Récupérer la branche Git
+local function git_branch()
+    local branch = vim.fn.system("git rev-parse --abbrev-ref HEAD 2>/dev/null | tr -d '\n'")
+
+    if string.len(branch) > 0 then
+        return branch
+    else
+        return ""
+    end
+end
+
+-- Customisation de la statusline
+local function statusline()
+    -- local set_color_1 = "%#PmenuSel#"
+
+    local branch = git_branch()
+
+    -- local set_color_2 = "%#LineNr#"
+
+    local file_name = "%F"
+
+    local modified = " %m %r "
+
+    local align_right = "%="
+
+    local fileencoding = "%{&fileencoding?&fileencoding:&encoding} "
+
+    local fileformat = "[%{&fileformat}] "
+
+    local filetype = "%y "
+
+    local percentage = "%p%% "
+
+    local linecol = " %l:%c "
+
+    return string.format(
+        "%s %s%s%s%s%s%s%s%s",
+        -- set_color_1,
+        branch,
+        -- set_color_2,
+        file_name,
+        modified,
+        align_right,
+        filetype,
+        fileencoding,
+        fileformat,
+        linecol,
+        percentage
+    )
+end
+
+vim.opt.statusline = statusline()
+
 -- Background et Foreground pour la statusline VIM sans lualine
 vim.cmd("highlight StatusLine ctermbg=1f1d2e guibg=#1f1d2e")
 vim.cmd("highlight StatusLine ctermfg=908caa guifg=#908caa")

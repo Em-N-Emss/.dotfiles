59c5df00c6a8852b5c8ba43e6f1dd0d70637dc97
Em-N-Ems <sengchanh.dylan@gmail.com>
2024-04-29 15:23:16 +0200

    feat(coding.lua): add surround & comment plugin

---
+++ .config/nvim/lua/plugins/coding.lua
@@ -0,0 +1,27 @@
+return {
+    -- Pour avoir ", {, [, `, ' qui se referme automatiquement
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equalent to setup({}) function
+    },
+
+    -- Pouvoir avoir les commentaires au bout des doigts grâce à "gc"
+    {
+        'numToStr/Comment.nvim',
+        opts = {
+            -- add any options here
+        },
+    },
+
+    -- Permettre de mettre en quote du texte existant
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        config = function()
+            require("nvim-surround").setup({})
+        end
+    }
+}

8c264a99d09a476bbbeee32a36f588f2a4bcd052
Em-N-Ems <sengchanh.dylan@gmail.com>
2024-05-12 19:27:26 +0200

    refactor(telescope.lua): change the keybind for project files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -26,7 +26,7 @@ return {
         })
 
         local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
+        vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 
         vim.keymap.set('n', '<leader>pg', builtin.git_files, {})
 

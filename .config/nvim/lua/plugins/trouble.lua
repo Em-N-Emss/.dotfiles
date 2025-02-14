b9402a3ac8f7cda35d5457c56d3eb990aa0d5233
Em-N-Ems <sengchanh.dylan@gmail.com>
2025-02-14 19:42:24 +0100

    refactor(trouble + lazy-lock): change [t next & add new lazy-lock

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -12,13 +12,13 @@ return {
         {
             "[t",
             function()
-                require("trouble").prev({ skip_groups = true, jump = true })
+                require("trouble").next({ skip_groups = true, jump = true })
             end,
         },
         {
             "]t",
             function()
-                require("trouble").next({ skip_groups = true, jump = true })
+                require("trouble").prev({ skip_groups = true, jump = true })
             end,
         },
     },

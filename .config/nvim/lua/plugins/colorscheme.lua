ccb2881f6ecad5bd403e68e15d3fd0c8e86b74d9
Em-N-Ems <sengchanh.dylan@gmail.com>
2025-02-25 11:20:42 +0100

    style(colorscheme.lua): rearange some lines for better visual

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -20,9 +20,9 @@ return {
                 TreesitterContextLineNumber = { bg = 'NONE' },
             }
         })
+        vim.cmd.colorscheme("rose-pine")
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.cmd.colorscheme("rose-pine")
     end
 }
 

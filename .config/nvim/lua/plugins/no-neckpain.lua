65c9ae9b4e5f40e62e740728e0a78aff84701409
Em-N-Emss <sengchanh.dylan@gmail.com>
2025-04-25 07:16:42 +0200

    feat(livepreview): better markdown preview as the other one is depreciated

--- .config/nvim/lua/plugins/no-neckpain.lua
+++ .config/nvim/lua/plugins/no-neckpain.lua
@@ -27,11 +27,25 @@ return {
     },
     -- Markdown plugin
     {
-		"iamcco/markdown-preview.nvim",
-		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-		ft = { "markdown" },
-		build = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	},
+        'brianhuster/live-preview.nvim',
+        dependencies = {
+            'nvim-telescope/telescope.nvim',
+        },
+    }
+ --    {
+	-- 	"iamcco/markdown-preview.nvim",
+	-- 	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	-- 	ft = { "markdown" },
+	-- 	build = function()
+	-- 		vim.fn["mkdp#util#install"]()
+	-- 	end,
+	-- },
+    -- {
+    --     'MeanderingProgrammer/render-markdown.nvim',
+    --     dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you prefer nvim-web-devicons
+    --     ---@module 'render-markdown'
+    --     ---@type render.md.UserConfig
+    --     opts = {
+    --     },
+    -- },
 }

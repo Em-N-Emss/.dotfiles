eaeaf48dbe1a15ffebfe60f95e20f0f88796b3ea
Em-N-Emss <sengchanh.dylan@gmail.com>
2025-05-17 23:27:00 +0200

    feat(komorebi.zsh): better kte function

--- .config/zsh/komorebi.zsh
+++ .config/zsh/komorebi.zsh
@@ -66,13 +66,10 @@ function kftys() {
 
 # Permet de kill tout ce qui est en rapport avec Komorebi
 function kte() {
-    # Attente limitée à 1 seconde pour la terminaison de Komorebi et whkd tant que les processus sont actifs
-    if [[ $(pwsh.exe -Command 'Get-Process Komorebi' 2>/dev/null) && $(pwsh.exe -Command 'Get-Process whkd' 2>/dev/null) ]]; then
-        # Byebye Komorebi
-        komorebic stop
-        pwsh.exe -Command 'taskkill /f /im komorebi.exe'
-        # Fermer aussi Whkd par précaution
-        pwsh.exe -Command 'taskkill /f /im whkd.exe'
+
+    # Si Komorebi est en cours de fonctionnement on le ferme
+    if [[ $(pwsh.exe -Command 'Get-Process Komorebi' 2>/dev/null) ]]; then
+        pwsh.exe -Command 'komorebic stop --whkd'
     fi
 
     # Ferme toutes les sessions Tmux sauf "Work"

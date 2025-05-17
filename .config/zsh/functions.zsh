9f5e1b6daf28e77a9cda61daf8ac4c8db76ef49a
Em-N-Emss <sengchanh.dylan@gmail.com>
2025-05-17 22:29:26 +0200

    fix(functions.zsh): adapt the typo

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -1,5 +1,5 @@
 source $HOME/.config/zsh/zsh_user_keybindings.zsh
-source $HOME/.config/zsh/komerebi.zsh
+source $HOME/.config/zsh/komorebi.zsh
 source $HOME/.config/zsh/fzf_change_directory.zsh
 
 # Execute key bindings function
